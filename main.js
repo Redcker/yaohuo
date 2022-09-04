0bc3e85ce74232a9d67becd6bd6ac09c47e2d364
Redcker <admin@yangyingqi.com>
2022-09-04 16:43:29 +0800

    删除无用导致报错的代码行

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
-import uView from "uview-ui";
-Vue.use(uView);
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
